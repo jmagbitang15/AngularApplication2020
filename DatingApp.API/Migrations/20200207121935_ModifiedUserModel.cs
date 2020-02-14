72bb183c9ce73bdfab2c759ecd1eee75146c9ae9
jmagbitang15 <josemarimagbitang@gmail.com>
2020-02-15 00:19:57 +0800

    End of Section 3

---
+++ DatingApp.API/Migrations/20200207121935_ModifiedUserModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class ModifiedUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TABLE Users");
+          
+            // migrationBuilder.AddColumn<string>(
+            //     name: "Username",
+            //     table: "Users",
+            //     nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
